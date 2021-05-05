1ea52c5260b1a7d5441ad88077f20c613f06b081
aref <arnodorian067@outlook.com>
2021-05-05 17:18:36 +0430

    finished footer

--- Components/Button.js
+++ Components/Button.js
@@ -9,13 +9,15 @@ export default function Button({ children, type, color }) {
                 return 'bg-amber-500 text-white'
             case 'transparent':
                 return 'bg-transparent text-black-500 hover:bg-black-100'
+            case 'border':
+                return 'bg-transparent border-2 border-indigo-500 text-indigo-500 transition duration-200 colors hover:bg-indigo-500 hover:text-white'
         }
     }
 
     return (
         <button
             type={type}
-            className={`rounded-full rounded-full py-3 px-8 text-white py-3 px-8 ${colorClass()}`}>
+            className={`rounded-full rounded-full py-3 px-8 py-3 px-8 ${colorClass()}`}>
             {children}
         </button>
     )
